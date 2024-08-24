03bdf7fb9e3acffde898f3629fa85a343957232b
paalamugan <paalamugan44@gmail.com>
2024-08-25 01:09:34 +0530

    chore: update env.ts to include VERCEL_ENV and fix skipValidation logic

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,8 @@
 import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
 
+import { VERCEL_ENV } from './next-helpers/next.constants';
+
 // Don't add NODE_ENV into T3 Env, it changes the tree-shaking behavior
 export const env = createEnv({
   /**
@@ -29,8 +31,8 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    MONGODB_URI: process.env.NEXT_MONGODB_URI,
-    REDIS_URL: process.env.NEXT_REDIS_URL,
+    MONGODB_URI: process.env.MONGODB_URI,
+    REDIS_URL: process.env.REDIS_URL,
     ANALYZE: process.env.ANALYZE,
 
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
@@ -45,7 +47,7 @@ export const env = createEnv({
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
    * useful for Docker builds.
    */
-  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+  skipValidation: !!VERCEL_ENV || !!process.env.SKIP_ENV_VALIDATION,
   /**
    * Makes it so that empty strings are treated as undefined. `SOME_VAR: z.string()` and
    * `SOME_VAR=''` will throw an error.
